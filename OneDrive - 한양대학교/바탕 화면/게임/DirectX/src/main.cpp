a41b1abc8dc382a9b6d1f26a0a62069d3a342df3
Winteradio <tot4766@hanyang.ac.kr>
2023-02-06 19:28:49 +0900

    Type : feature Branch : DirectX Auther : Lee Wonmock Date : 2023-02-06 Desc : Main Loop

--- OneDrive - 한양대학교/바탕 화면/게임/DirectX/src/main.cpp
+++ OneDrive - 한양대학교/바탕 화면/게임/DirectX/src/main.cpp
@@ -1,4 +1,18 @@
 #include <windows.h>
+#include <stdio.h>
+
+LRESULT CALLBACK WndProc( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam )
+{
+	switch( msg )
+	{
+		case WM_CLOSE :
+		{
+			PostQuitMessage( 69 );
+			break;
+		}
+	}
+	return DefWindowProc( hWnd, msg, wParam, lParam );
+}
 
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
@@ -12,7 +26,7 @@ int CALLBACK WinMain(
 	WNDCLASSEX wc = {0};
 	wc.cbSize = sizeof( wc );
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -33,7 +47,28 @@ int CALLBACK WinMain(
 		200, 200, 640, 480,
 		nullptr, nullptr, hInstance, nullptr
 		);
+
+	// Show the damn Window
 	ShowWindow( hWnd, SW_SHOW );
-	while ( true );
+
+	// Message pup
+	MSG msg;
+	BOOL gResult;
+	while ( ( gResult = GetMessage( &msg, nullptr, 0, 0 ) )  > 0 )
+	{
+		TranslateMessage( &msg );
+		DispatchMessage( &msg );
+	}
+
+	if ( gResult == -1 )
+	{
+		return -1;
+	}
+	else
+	{
+		msg.wParam;
+	}
+
+	printf(" Done \n");
 	return 0;
 }
